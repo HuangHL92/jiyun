8b0073094f7ecc4092a5d4889876aca932d68675
梁涛 <18651018439@153.com>
2019-08-17 11:12:57 +0800

    部署环境配置文件上传

--- ruoyi-admin/stop.bat
+++ ruoyi-admin/stop.bat
@@ -5,6 +5,8 @@ echo.
 
 set port=8081
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":%port%"') do (
-    echo kill the process %%m who use the port %port%
-    taskkill /F /pid %%m
+    tasklist /FI "PID eq %%m"|find /i "PID" && (
+            echo PID:%%m 运行中,kill the process [%%m] who use the port [%port%]
+            taskkill /F /pid %%m
+            ) || echo PID:%%m 未运行
 )
