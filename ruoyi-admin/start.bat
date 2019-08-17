d81f3c303ecd7128e11818e095061a167cd9d659
梁涛 <18651018439@153.com>
2019-08-17 10:39:53 +0800

    部署环境配置文件上传

--- ruoyi-admin/start.bat
+++ ruoyi-admin/start.bat
@@ -1,15 +1,15 @@
 @echo off
 echo.
-echo ==========[统一身份认证平台] 启动项目==========
+echo ========== [统一身份认证平台] 启动项目 ==========
 echo.
 
 cd %~dp0
 cd ../ruoyi-admin/target
 
-#set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
-#java -jar %JAVA_OPTS% tysfrzpt-admin.jar
+%set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m%
+%java -jar %JAVA_OPTS% tysfrzpt-admin.jar%
 
-java -jar tysfrzpt-admin.jar
+java -jar -Dspring.profiles.active=prod tysfrzpt-admin.jar
 
 cd bin
 pause
