e4afeedc41d01c660f1463e51305569e311a3cf1
梁涛 <18651018439@153.com>
2019-08-17 11:51:10 +0800

    部署环境配置文件上传

--- ruoyi-admin/stop.bat
+++ ruoyi-admin/stop.bat
@@ -1,17 +1,25 @@
-@echo off
+@echo off & color 3d & setlocal enabledelayedexpansion
+  ::ipconfig>ip.txt
+
 echo.
 echo ========== [统一身份认证平台] 关闭已启动项目 ==========
 echo.
-
-setlocal enabledelayedexpansion
 set /p port=8081
-for /f "tokens=1-5" %%a in ('netstat -ano ^| find ":%port%"') do (
-    if "%%e%" == "" (
-        set pid=%%d
-    ) else (
-        set pid=%%e
+netstat -aon |findstr %port%>pid.txt
+
+     for /f "delims=" %%a in (pid.txt) do (
+
+      for /f "tokens=1* delims=:" %%i in ('call echo %%a^|find /i "TCP"') do (
+         echo %%a
+     ::读取出内容过滤后,写入另一个记事本中
+     rem Echo %%a>>"text.txt"
+        )
     )
-    echo !pid!
-    taskkill /f /pid !pid!
-)
-pause
+rem 读取文件中内容
+set /P OEM=<pid.txt
+
+rem 截取文件中的字符串
+
+echo  %OEM:~71,76%
+
+taskkill /f /pid %OEM:~71,76%
