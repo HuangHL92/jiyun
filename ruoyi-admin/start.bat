fabc813bb34befaee7a7dd61e6476c6c9bd665ae
梁涛 <18651018439@153.com>
2019-08-17 11:58:01 +0800

    部署环境配置文件上传

--- ruoyi-admin/start.bat
+++ ruoyi-admin/start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo.
-echo ========== [统一身份认证平台] 启动项目 ==========
+echo ========== [统一身份认证平台] 启动项目开始 ==========
 echo.
 
 cd %~dp0
@@ -9,4 +9,6 @@ cd ../ruoyi-admin/target
 ::set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 ::java -jar %JAVA_OPTS% tysfrzpt-admin.jar
 
-start java -jar -Dspring.profiles.active=prod tysfrzpt-admin.jar
+set BUILD_ID=dontKillMe
+start javaw -jar -Dspring.profiles.active=prod tysfrzpt-admin.jar
+echo ========== [统一身份认证平台] 启动项目结束 ==========
--- ruoyi-admin/stop.bat
+++ ruoyi-admin/stop.bat
@@ -1,25 +1,17 @@
-@echo off & color 3d & setlocal enabledelayedexpansion
-  ::ipconfig>ip.txt
-
+@echo off
 echo.
-echo ========== [统一身份认证平台] 关闭已启动项目 ==========
+echo ========== [统一身份认证平台] 关闭之前项目 ==========
 echo.
-set /p port=8081
-netstat -aon |findstr %port%>pid.txt
 
-     for /f "delims=" %%a in (pid.txt) do (
-
-      for /f "tokens=1* delims=:" %%i in ('call echo %%a^|find /i "TCP"') do (
-         echo %%a
-     ::读取出内容过滤后,写入另一个记事本中
-     rem Echo %%a>>"text.txt"
-        )
-    )
-rem 读取文件中内容
-set /P OEM=<pid.txt
-
-rem 截取文件中的字符串
-
-echo  %OEM:~71,76%
-
-taskkill /f /pid %OEM:~71,76%
+setlocal enabledelayedexpansion
+set port=8081
+for /f "tokens=1-5" %%a in ('netstat -ano ^| find ":%port%"') do (
+ if "%%e%" == "" (
+     set pid=%%d
+ ) else (
+     set pid=%%e
+ )
+ echo !pid!
+ taskkill /f /pid !pid!
+)
+pause
