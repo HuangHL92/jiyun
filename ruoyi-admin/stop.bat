e9c2094dfc5ad85856923c1124e54bfcc5b73975
梁涛 <18651018439@153.com>
2019-08-17 11:45:38 +0800

    部署环境配置文件上传

--- ruoyi-admin/stop.bat
+++ ruoyi-admin/stop.bat
@@ -3,6 +3,15 @@ echo.
 echo ========== [统一身份认证平台] 关闭已启动项目 ==========
 echo.
 
-@echo off
-set port=8081
-for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":%port%"') do taskkill /pid %%m
+setlocal enabledelayedexpansion
+set /p port=8081
+for /f "tokens=1-5" %%a in ('netstat -ano ^| find ":%port%"') do (
+    if "%%e%" == "" (
+        set pid=%%d
+    ) else (
+        set pid=%%e
+    )
+    echo !pid!
+    taskkill /f /pid !pid!
+)
+pause
