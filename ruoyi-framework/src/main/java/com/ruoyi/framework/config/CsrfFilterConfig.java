526f77fc6ac029ff8b7f751d0665e2ce6afc75f1
梁涛 <18651018439@153.com>
2019-08-21 11:48:02 +0800

    系统优化：防止CSRF攻击

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/CsrfFilterConfig.java
@@ -0,0 +1,24 @@
+package com.ruoyi.framework.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+/**
+ * CSRF Filter
+ *
+ * @author tao.liang
+ * @date 2019年8月20日
+ */
+@Configuration
+public class CsrfFilterConfig {
+    @Bean
+    public FilterRegistrationBean csrfFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new CsrfFilter(new HttpSessionCsrfTokenRepository()));
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+}
