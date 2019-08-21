a2bb5ca34882b0b4cc858f2fa6b1261df82bbffc
梁涛 <18651018439@153.com>
2019-08-21 16:00:54 +0800

    端口調整

--- bin/stop.bat
+++ bin/stop.bat
@@ -4,7 +4,7 @@ echo ========== [统一身份认证平台] 关闭之前项目 ==========
 echo.
 
 setlocal enabledelayedexpansion
-set port=8081
+set port=9000
 set pid=-1
 for /f "tokens=1-5" %%a in ('netstat -ano ^| find ":%port%"') do (
  if !pid! neq %%e (
@@ -18,7 +18,7 @@ for /f "tokens=1-5" %%a in ('netstat -ano ^| find ":%port%"') do (
  )
 )
 
-set port=8082
+set port=9001
 set pid=-1
 for /f "tokens=1-5" %%a in ('netstat -ano ^| find ":%port%"') do (
  if !pid! neq %%e (
